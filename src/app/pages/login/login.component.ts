693b8515c2c85bb88e2a3f3ea4d9b2b5f9d247ed
Luis Dominguez <luis-dominguez1994@hotmail.com>
2017-01-07 15:47:03 -0400

    select method on login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import {Component, ViewEncapsulation} from '@angular/core';
 import {FormGroup, AbstractControl, FormBuilder, Validators} from '@angular/forms';
+import {BackandService} from 'angular2bknd-sdk';
 
 @Component({
   selector: 'login',
@@ -9,12 +10,16 @@ import {FormGroup, AbstractControl, FormBuilder, Validators} from '@angular/form
 })
 export class Login {
 
+  //TODO: validate username or email with the database results
+
   public form:FormGroup;
   public email:AbstractControl;
   public password:AbstractControl;
   public submitted:boolean = false;
+  public username:string;
+  private items:any;
 
-  constructor(fb:FormBuilder) {
+  constructor(fb:FormBuilder,private backandService:BackandService) {
     this.form = fb.group({
       'email': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
       'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])]
@@ -22,13 +27,40 @@ export class Login {
 
     this.email = this.form.controls['email'];
     this.password = this.form.controls['password'];
+    
   }
 
   public onSubmit(values:Object):void {
+    console.log('presione el boton');
     this.submitted = true;
-    if (this.form.valid) {
-      // your code goes here
-      // console.log(values);
+    
+    if (this.form.valid) {// luego de que se llene el formulario
+      let filter =//filtro para el select (where clause)
+          [
+            {
+              fieldName: 'username',//nombre del campo
+              operator: 'contains',//operador, si es un numero se usa equals, si es string contains, ahi mas variantes
+              //remover comentario cuando se utilize el campo del formulario
+              // value: this.username
+              value: 'jose'//valor
+            }
+          ]
+      ;
+      //Los metodos disponibles estan definidos en node_modules/angular2bknd-sdk/backandService
+      this.backandService.getList('users',null,null,filter)//nombre de la tabla,pagesiz,pagenumber,filtro
+            .subscribe(
+                data => {
+                    console.log('lista de usuarios',data);
+                    this.items = data;
+                },
+                err => this.backandService.logError(err),
+                ()=> this.loginUser()
+            );  
     }
   }
+  loginUser(){
+     
+    }
+
 }
+
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -7,6 +7,7 @@ import { Login } from './login.component';
 import { routing }       from './login.routing';
 
 
+
 @NgModule({
   imports: [
     CommonModule,
