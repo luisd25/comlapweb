33a48e3fc3cff8bab4da6aeb609d6b37620f476e
Luis Dominguez <luis-dominguez1994@hotmail.com>
2017-01-10 01:09:17 -0400

    functional mapping

--- src/app/pages/maps/components/googleMaps/googleMaps.component.ts
+++ src/app/pages/maps/components/googleMaps/googleMaps.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef} from '@angular/core';
-import {GoogleMapsLoader} from './googleMaps.loader';
+import {BackandService} from 'angular2bknd-sdk';
+// import {GoogleMapsLoader} from './googleMaps.loader';
 
 @Component({
   selector: 'google-maps',
@@ -7,20 +8,80 @@ import {GoogleMapsLoader} from './googleMaps.loader';
   template: require('./googleMaps.html'),
 })
 export class GoogleMaps {
-
-  constructor(private _elementRef:ElementRef) {
+  public hospitalList: any;
+ 
+  constructor(private _elementRef:ElementRef,private backandService:BackandService) {
   }
 
   ngAfterViewInit() {
-    let el = this._elementRef.nativeElement.querySelector('.google-maps');
+    
+    // let map = this._elementRef.nativeElement.querySelector('.google-maps');
 
     // TODO: do not load this each time as we already have the library after first attempt
-    GoogleMapsLoader.load((google) => {
-      new google.maps.Map(el, {
-        center: new google.maps.LatLng(44.5403, -78.5463),
-        zoom: 8,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      });
+    // GoogleMapsLoader.load((google) => {
+    //   new google.maps.Map(map, {
+    //     center: new google.maps.LatLng(44.5403, -78.5463),
+    //     zoom: 8,
+    //     mapTypeId: google.maps.MapTypeId.ROADMAP
+    //   });
+    // });
+        navigator.geolocation.getCurrentPosition((position)=>{
+      console.log('geo: ',position.coords.latitude,position.coords.longitude);
+      var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      map.setCenter(latLng)
+
     });
+        var mapProp = {
+            center: new google.maps.LatLng(18.4098742 , -70.1198232),
+            zoom: 8,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(this._elementRef.nativeElement.querySelector('.google-maps'), mapProp);
+
+        
+
+        // To add the marker to the map, call setMap();
+        
+        let filter =
+                [
+                    {
+                    fieldName: "Provincia",
+                    operator: "contains",
+                    value: "SANTO DOMINGO"
+                    }
+                ]
+            ;
+
+            this.backandService.getList('hospital',280,null,filter)
+           .subscribe(
+               data => {
+                   console.log(data);
+                   this.hospitalList = data;
+               },
+               err => this.backandService.logError(err),
+               ()=> {
+                    for(let location of this.hospitalList){
+                        var marker = new google.maps.Marker({
+                          position: new google.maps.LatLng(location.latitude,location.longitude),
+                          title:location.NombreCentro,
+                          animation: google.maps.Animation.DROP,
+                          map:map
+                        });
+                    }
+                      // marker.setMap(map);
+                  }
+
+            
+           );
+
   }
+  ngOnInit() {
+
+
+        
+    }
+
+    loadhospital(){
+      
+    }
 }
